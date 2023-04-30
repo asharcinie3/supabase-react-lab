798a3bc6caba94595aac2a1af70d8c985efda518
asharcinie3 <arcinie3@msu.edu>
2023-04-30 01:45:56 -0400

    fix error

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { supabase } from './supabaseClient';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function Library() {
